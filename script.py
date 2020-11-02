cc7db91633c50e0e29db8103ea73e69feaa95db9
Alesc <PedroAlexandreftw@gmail.com>
2020-11-02 16:56:47 +0000

    Fixed disconnected devices not showing

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import nmap
 import time
 from pushbullet import Pushbullet
-pb = Pushbullet('TOKEN')
+pb = Pushbullet('o.CkGvbNmkW5biq0syYmnRbe4bhJgz1OFD')
 
 n = 0
 hosts_list = []
@@ -13,14 +13,16 @@ while True:
 
     nm = nmap.PortScanner()
     nm.scan(hosts='192.168.1.1/24', arguments='-R -sP -PE')
-    hosts_list = [(nm[x]['hostnames'][0]['name'],x, nm[x]['status']['state']) for x in nm.all_hosts()]
+    hosts_list = [(nm[x]['hostnames'][0]['name'],x) for x in nm.all_hosts()]
     
     for x in hosts_list :
-            if(x not in last_list):
-                print(x[0] + " CONNECTED")
-                """ push = pb.push_note("Connected", "IP CONNECTED") """
+        if(x not in last_list):
+            print(x[0] + "  " + x[1] + " CONNECTED")
+            """ push = pb.push_note("Connected", "IP CONNECTED") """
+            
+    for x in last_list : 
             if(x not in hosts_list):
-                print(x[0] + " DISCONNECTED")
+                print(x[0] + "  " + x[1] + " DISCONNECTED")
                 """ push = pb.push_note("Disconnected", "IP DISCONNECTED") """
     
     last_list = hosts_list
